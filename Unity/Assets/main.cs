12a6a82c62cc6c22b0f0cd8725c042b22c6e9bee
Patrick Andersen <patrick@emsmith.net>
2014-12-04 14:33:52 -0800

    Implemented grid
    Implemented internal grid system in BoardController and created AddHex()
    function.

---
+++ Unity/Assets/main.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class main : MonoBehaviour {
+
+    BoardController board; 
+    
+	void Start () {
+
+        board = (BoardController)GameObject.Find("background").GetComponent<MonoBehaviour>();
+        board.AddHex(0, 0, 0);
+
+	}
+
+}
