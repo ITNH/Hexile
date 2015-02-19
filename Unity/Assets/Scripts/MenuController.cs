38ac6adcfcdde060ccec948523a45c84ce2fbac7
Patrick Andersen <patrick@emsmith.net>
2015-02-18 21:55:34 -0500

    Laid groundwork for main menu
    Images are prepared, scene and script are created, we are go for launch!

---
+++ Unity/Assets/Scripts/MenuController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Controller responsible for operating the main menu
+public class MenuController : MonoBehaviour {
+
+	void Start () {
+
+
+	
+	}
+
+	void Update () {
+
+
+	
+	}
+
+}
