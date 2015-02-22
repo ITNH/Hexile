41c3ee2fc711dff5967b7d27a9061580dbdda24b
Patrick Andersen <patrick@emsmith.net>
2015-02-21 23:54:39 -0500

    Music!
    It's a little short, but it works! Plus, we can change it later.

---
+++ Unity/Assets/Scripts/MusicController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicController : MonoBehaviour {
+
+    private AudioSource intro;
+    private AudioSource loop;
+
+	void Start () {
+	
+        AudioSource[] sources = gameObject.GetComponents<AudioSource>();
+
+        intro = sources[0];
+        loop = sources[1];
+
+    }
+	
+	void Update () {
+
+        if (!intro.isPlaying && !loop.isPlaying)
+        {
+
+            loop.Play();
+
+        }
+	
+	}
+}
